19228c9f026ecfb1a7fb00eee4fac0da9c7cee39
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-05-20 11:02:45 +0530

    create  the constatnt code

---
+++ lib/core/constant/app_sizes.dart
@@ -0,0 +1,6 @@
+class AppSizes {
+  static const double padding = 16;
+  static const double radius = 10;
+  static const double buttonHeight = 55;
+  static const double textFieldHeight = 60;
+}
---
+++ lib/core/constant/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const String login = "Login";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String users = "Users";
+}
